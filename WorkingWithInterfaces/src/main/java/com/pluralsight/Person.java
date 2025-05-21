3043ccebd71ca49f14583251faa4d4259e2ba601
Phoniix <seanasb52058@gmail.com>
2025-05-21 12:18:25 -0500

    Skeleton commit

---
+++ WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class Person implements Comparable{
+    String firstName;
+    String lastName;
+    int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Person other) {
+            int lastNameCompare = this.lastName.compareToIgnoreCase(other.lastName);
+            if (lastNameCompare != 0) return lastNameCompare;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return lastName + ", " + firstName + ", " + age;
+    }
+
+    private enum factor {
+        AGE,
+        LAST_NAME,
+        FIRST_NAME;
+    }
+
+}
